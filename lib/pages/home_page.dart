e9613f96cf9bc915a9bd363e1d6d0fe2218eff36
Reubencfernandes <18reuchagasfernandes@gmail.com>
2024-06-27 23:03:52 +0530

    Home Page Done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+
+Map<String, double> dataMap = {
+  "Study": 5,
+  "Work": 3,
+  "Sleep": 2,
+  "Hobbies": 2,
+};
+List<Color> colorList = [
+  Colors.blue,
+  Colors.green,
+  Colors.yellow,
+  Colors.red,
+];
 
 class Homestuff extends StatelessWidget {
   const Homestuff({super.key});
@@ -9,8 +23,9 @@ class Homestuff extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       body: Container(
-        margin: const EdgeInsets.only(top: 70, left: 20,right: 20),
+        margin: const EdgeInsets.only(top: 70, left: 20, right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -18,7 +33,8 @@ class Homestuff extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(50.0), // Adjust the radius as needed
+                  borderRadius: BorderRadius.circular(
+                      50.0), // Adjust the radius as needed
                   child: SizedBox(
                     width: 50.0, // Adjust the width as needed
                     height: 50.0, // Adjust the height as needed
@@ -32,9 +48,14 @@ class Homestuff extends StatelessWidget {
                 const Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Hello,", style: TextStyle(fontFamily: 'inter', color: Colors.black)),
+                    Text("Hello,",
+                        style: TextStyle(
+                            fontFamily: 'inter', color: Colors.black)),
                     Text("Reuben Fernandes",
-                        style: TextStyle(fontFamily: 'inter', fontSize: 20, fontWeight: FontWeight.bold)),
+                        style: TextStyle(
+                            fontFamily: 'inter',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold)),
                   ],
                 ),
               ],
@@ -43,60 +64,211 @@ class Homestuff extends StatelessWidget {
             Expanded(
               child: ListView(
                 children: [
-                  Text("Current Task",
-                      style: TextStyle(fontFamily: 'inter', fontSize: 20, fontWeight: FontWeight.bold)),
+                  const Text(" Current Task",
+                      style: TextStyle(
+                          fontFamily: 'inter',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
                   Card(
-                    margin: EdgeInsets.only(left: 5, right: 5),
+                    margin: const EdgeInsets.only(left: 5, right: 5, top: 10),
                     child: Padding(
-                      padding: EdgeInsets.only(top: 15, bottom: 30, left: 12, right: 12),
+                      padding: const EdgeInsets.only(
+                          top: 15, bottom: 30, left: 12, right: 12),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           ElevatedButton(
                             onPressed: () {}, // Non-null callback
                             style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.pink,
+                              backgroundColor: Colors.pink[100],
                             ),
                             child: Text(
                               "Work",
-                              style: TextStyle(color: Colors.black),
+                              style: TextStyle(
+                                color: Colors.red[900],
+                                fontFamily: 'inter',
+                                fontSize: 14,
+                              ),
                             ),
                           ),
-                          SizedBox(height: 5),
-                          Text("Design an android App Using Flutter",
-                              style: TextStyle(fontFamily: 'inter', fontSize: 20, fontWeight: FontWeight.bold)),
-                          SizedBox(height: 5),
+                          const SizedBox(height: 5),
+                          const Text("Design an android App Using Flutter",
+                              style: TextStyle(
+                                  fontFamily: 'inter',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 10),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text("Progress"),
-                              Text("35%"),
+                              Text("  Progress",
+                                  style: TextStyle(
+                                      fontFamily: 'inter',
+                                      color: Colors.grey[600])),
+                              const Text("35%",
+                                  style: TextStyle(
+                                      fontFamily: 'inter',
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
                             ],
                           ),
-                    Padding(
-                      padding: EdgeInsets.all(5.0),
-                          child: new LinearPercentIndicator(
-                            lineHeight: 15.0,
-                            percent: 0.8,
-                            progressColor: Colors.blue,
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: LinearPercentIndicator(
+                                lineHeight: 15.0,
+                                percent: 0.8,
+                                progressColor: Colors.black,
+                                backgroundColor: Colors.grey[300],
+                                barRadius: const Radius.circular(10),
+                              ),
+                            ),
                           ),
-                      ),
-                          SizedBox(height: 10,),
+                          const SizedBox(
+                            height: 15,
+                          ),
                           Row(
-
                             mainAxisAlignment: MainAxisAlignment.end,
                             crossAxisAlignment: CrossAxisAlignment.end,
-
                             children: [
-
-                              Icon(Icons.calendar_month,color: Colors.grey),
-                              Text("Tue,4 May 2024")
+                              const Icon(Icons.calendar_month,
+                                  color: Colors.grey),
+                              Text(
+                                "Tue, 4 May 2024",
+                                style: TextStyle(
+                                    fontFamily: 'inter',
+                                    color: Colors.grey[600],
+                                    fontWeight: FontWeight.w600),
+                              )
                             ],
                           )
                         ],
                       ),
-
                     ),
+                  ),
+                  const SizedBox(height: 30),
+                  const Text(" Analysis of Tasks",
+                      style: TextStyle(
+                          fontFamily: 'inter',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Card(
+                    margin: const EdgeInsets.only(left: 5, right: 5, top: 10),
+                    child: Padding(
+                        padding: const EdgeInsets.only(
+                            top: 15, bottom: 30, left: 12, right: 12),
+                        child: PieChart(
+                          dataMap: dataMap,
+                          animationDuration: const Duration(milliseconds: 800),
+                          colorList: colorList,
+                          initialAngleInDegree: 0,
+                          chartType: ChartType.disc,
+                          ringStrokeWidth: 32,
+                          legendOptions: const LegendOptions(
+                            showLegendsInRow: false,
+                            legendPosition: LegendPosition.right,
+                            showLegends: true,
+                            legendShape: BoxShape.circle,
+                            legendTextStyle: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontFamily: 'inter',
+                            ),
+                          ),
+                          chartValuesOptions: const ChartValuesOptions(
+                            showChartValueBackground: true,
+                            showChartValues: true,
+                            showChartValuesInPercentage: true,
+                            showChartValuesOutside: false,
+                            decimalPlaces: 1,
+                          ),
+                          // gradientList: ---To add gradient colors---
+                          // emptyColorGradient: ---Empty Color gradient---
+                        )),
+                  ),
+                  const SizedBox(height: 20),
+                  Card(
+                    margin: const EdgeInsets.only(left: 5, right: 5, top: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 15, bottom: 30, left: 12, right: 12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const SizedBox(height: 5),
+                          const Text("Progress",
+                              style: TextStyle(
+                                  fontFamily: 'inter',
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("  Time Left This Month",
+                                  style: TextStyle(
+                                      fontFamily: 'inter',
+                                      color: Colors.grey[600])),
+                              const Text("35%",
+                                  style: TextStyle(
+                                      fontFamily: 'inter',
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: LinearPercentIndicator(
+                                lineHeight: 15.0,
+                                percent: 0.8,
+                                progressColor: Colors.red[400],
+                                backgroundColor: Colors.grey[300],
+                                barRadius: const Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("  Time Left This Year",
+                                  style: TextStyle(
+                                      fontFamily: 'inter',
+                                      color: Colors.grey[600])),
+                              const Text("35%",
+                                  style: TextStyle(
+                                      fontFamily: 'inter',
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: LinearPercentIndicator(
+                                lineHeight: 15.0,
+                                percent: 0.8,
+                                progressColor: Colors.blue[400],
+                                backgroundColor: Colors.grey[300],
+                                barRadius: const Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
                   ),
                 ],
               ),
