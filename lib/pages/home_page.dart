3242282022a6e9dcdad41419b506d9f553a5e732
Reubencfernandes <18reuchagasfernandes@gmail.com>
2024-06-27 18:33:30 +0530

    started With Front End

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+
 class Homestuff extends StatelessWidget {
   const Homestuff({super.key});
 
@@ -9,7 +10,7 @@ class Homestuff extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: const EdgeInsets.only(top: 70, left: 20),
+        margin: const EdgeInsets.only(top: 70, left: 20,right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -17,8 +18,7 @@ class Homestuff extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(
-                      50.0), // Adjust the radius as needed
+                  borderRadius: BorderRadius.circular(50.0), // Adjust the radius as needed
                   child: SizedBox(
                     width: 50.0, // Adjust the width as needed
                     height: 50.0, // Adjust the height as needed
@@ -32,14 +32,9 @@ class Homestuff extends StatelessWidget {
                 const Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Hello,",
-                        style: TextStyle(
-                            fontFamily: 'inter', color: Colors.black)),
+                    Text("Hello,", style: TextStyle(fontFamily: 'inter', color: Colors.black)),
                     Text("Reuben Fernandes",
-                        style: TextStyle(
-                            fontFamily: 'inter',
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold)),
+                        style: TextStyle(fontFamily: 'inter', fontSize: 20, fontWeight: FontWeight.bold)),
                   ],
                 ),
               ],
@@ -49,33 +44,27 @@ class Homestuff extends StatelessWidget {
               child: ListView(
                 children: [
                   Text("Current Task",
-                      style: TextStyle(
-                          fontFamily: 'inter',
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold)),
+                      style: TextStyle(fontFamily: 'inter', fontSize: 20, fontWeight: FontWeight.bold)),
                   Card(
                     margin: EdgeInsets.only(left: 5, right: 5),
                     child: Padding(
-                      padding: EdgeInsets.only(
-                          top: 10, bottom: 50, left: 10, right: 10),
+                      padding: EdgeInsets.only(top: 15, bottom: 30, left: 12, right: 12),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           ElevatedButton(
-                              onPressed: null,
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.black,
-                              ),
-                              child: Text(
-                                "Work",
-                                style: TextStyle(color: Colors.redAccent),
-                              )),
+                            onPressed: () {}, // Non-null callback
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.pink,
+                            ),
+                            child: Text(
+                              "Work",
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
                           SizedBox(height: 5),
                           Text("Design an android App Using Flutter",
-                              style: TextStyle(
-                                  fontFamily: 'inter',
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold)),
+                              style: TextStyle(fontFamily: 'inter', fontSize: 20, fontWeight: FontWeight.bold)),
                           SizedBox(height: 5),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -84,15 +73,29 @@ class Homestuff extends StatelessWidget {
                               Text("35%"),
                             ],
                           ),
-                          new LinearPercentIndicator(
-                            width: 100.0,
-                            lineHeight: 8.0,
-                            percent: 0.9,
+                    Padding(
+                      padding: EdgeInsets.all(5.0),
+                          child: new LinearPercentIndicator(
+                            lineHeight: 15.0,
+                            percent: 0.8,
                             progressColor: Colors.blue,
                           ),
-                          Text("Text 4"),
+                      ),
+                          SizedBox(height: 10,),
+                          Row(
+
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+
+                            children: [
+
+                              Icon(Icons.calendar_month,color: Colors.grey),
+                              Text("Tue,4 May 2024")
+                            ],
+                          )
                         ],
                       ),
+
                     ),
                   ),
                 ],
