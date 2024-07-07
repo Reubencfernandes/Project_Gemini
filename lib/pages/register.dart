859438560a47e4542f9dc841eb07e4ceb3084931
Reubencfernandes <18reuchagasfernandes@gmail.com>
2024-07-07 17:21:50 +0530

    making changes

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:isar/isar.dart';
 
 class CreateAccount extends StatefulWidget {
@@ -10,7 +11,34 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  DateTime now = DateTime.now().toUtc();
+  DateTime selectedDate = DateTime.now().toUtc();
+  late DateTime lastDate = DateTime(now.year + 1, now.month, now.day);
+  late String formattedMonth = DateFormat('MMMM dd').format(selectedDate);
   final TextEditingController nameController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: now,
+        lastDate: lastDate,
+        builder: (BuildContext context, Widget? child) {
+          return Theme(
+              data: ThemeData.light().copyWith(
+                colorScheme: const ColorScheme.light(
+                  primary: Colors.black87,
+                ),
+              ),
+              child: child!);
+        });
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        formattedMonth = DateFormat('MMMM dd, yyyy').format(selectedDate);
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -23,43 +51,36 @@ class _CreateAccountState extends State<CreateAccount> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: const EdgeInsets.only(top: 50, left: 10, right: 10),
+        margin: const EdgeInsets.only(top: 50, left: 20, right: 20),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Create An Account "),
-            Expanded(
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  hintText: 'Reuben',
-                ),
-                maxLines: 1,
+            const SizedBox(height: 20), // Add space between elements
+            Text("Enter Your Name"),
+            const SizedBox(height: 10), // Add space between elements
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'Reuben',
+              ),
+              maxLines: 1,
+            ),
+            const SizedBox(height: 20), // Add space between elements
+            Text("Enter Your Birthday"),
+            const SizedBox(height: 10), // Add space between elements
+            TextField(
+              onTap: () => _selectDate(context),
+              readOnly: true,
+              decoration: InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                border: InputBorder.none,
+                hintText: formattedMonth,
               ),
             ),
-            Expanded(
-              child: TextField(
-                controller: nameController,
-
-                decoration: const InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  hintText: 'Reuben',
-                ),
-                maxLines: 1,
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
-                child: Text(
-                  "Create Account",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontFamily: 'Bebas Neue',
-                      color: Colors.black),
-                ))
           ],
         ),
       ),
